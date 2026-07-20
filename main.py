564984ad07ffa0d937639ca3a8f660ed9db92fb1
Muhammad Yar Khan <muhammadyarkhan31@gmail.com>
2026-07-20 12:41:15 +0500

    Implement data loading and view endpoint
    Added data loading function and endpoint to view data.

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from fastapi import FastAPI
 
 ap = FastAPI()
 
+def load_data():
+  with open ("patients.json", "r") as f:
+       data = json.laod()
+    return data
+
+@app.get("/view")
+def view_all():
+  data = load_data()
+return data
