fbbadffc1d0450c9c2efa4fab16c62a206d82bac
yarkhano <muhammadyarkhan31@gmail.com>
2026-07-21 12:02:53 +0500

    created endpoint with path parmeters for specific patient

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Path
 import json
 import uvicorn
 
@@ -9,10 +9,21 @@ def load_data():
     data = json.load(f)
     return data
 
+#to see all patients data
 @app.get("/view")
 def view_all():
     data = load_data()
     return data
+
+
+#to see specific patient data
+@app.get("/patient/{patient_id}")
+def get_patient(patient_id: str = Path(...,description="Patient ID in DB",example="P001")):
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    return data[patient_id]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
