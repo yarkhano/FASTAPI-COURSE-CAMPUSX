c8875f3cc632d0e461f9956c7d92fac5adcb4d76
yarkhano <muhammadyarkhan31@gmail.com>
2026-07-22 20:20:35 +0500

    working on field and Annotated

--- pydantic_tutorial.py
+++ pydantic_tutorial.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel,EmailStr,AnyUrl
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,Annotated
 from typing import List,Dict,Optional
+
 
 class Patient(BaseModel):
     name: str
-    age: int
+    age: int = Field(gt=0,lt=120)
     email: EmailStr
+    weight:Annotated[float,Field(gt=0,strict=True)] #striict ensure value must be string if =True
     linkedin_url: AnyUrl
-    allergies: Optional[List[str]] = None
+    allergies: Annotated[Optional[List[str]],Field(default=None,max_length=5)]
     contact: Dict[str,str]
+
 
 def insert_patient(patient: Patient):
     print(patient.name)
